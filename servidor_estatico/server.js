df40105c89b1adc498d31af3ab294e321721b9d2
Murillo Prevelato <prevelatomurillo@gmail.com>
2025-05-10 12:31:38 -0300

    servidor rodando

--- servidor_estatico/server.js
+++ servidor_estatico/server.js
@@ -3,15 +3,16 @@ const path = require('path');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-server.listen(80);
 
+// Define o diretório onde estão os arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Define o diretório onde estão os arquivos 
-app.use(express.static(path.join(__dirname, '../public')));
-
+// Rota principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'home.html'));
-  });
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
 
 // Inicia o servidor na porta 80
-console.log("servidor rodando")
+server.listen(80, () => {
+    console.log("Servidor rodando em http://localhost");
+});
